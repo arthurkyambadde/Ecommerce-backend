d2116a79a42b72bd544bfd9f4d442453687b8c93
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-07-14 09:23:00 +0300

    (feat): use express for routing in the application

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(5000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-products", (req, res, next) => {
+  res.send(`<form action="/product" method="POST">
+  <input type="text" name="title">
+  <button type="submit"></button>
+  </form>`);
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send(`<H1>Hello world</H1>`);
+});
+
+app.listen(4000);
