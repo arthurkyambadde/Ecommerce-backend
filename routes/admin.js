f98819f702410599ce281d41924bbcf4220a74e2
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-07-16 13:08:26 +0300

    (feat): add css to the html templates

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,17 @@ const path = require("path");
 
 const router = express.Router();
 
-router.post("/add-products", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+const products = [];
 
-router.get("/add-products", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
-module.exports = router;
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
